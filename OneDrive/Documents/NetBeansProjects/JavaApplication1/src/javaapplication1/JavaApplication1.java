076c907666901485fc0b80f40924f538eba64edc
Aruna <guragainaruna@gmail.com>
2024-07-29 22:06:11 +0545

    updated sprint1

---
+++ OneDrive/Documents/NetBeansProjects/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+import javaapplication1.firstswing.view.SignupScreen;
+import javaapplication1.firstswing.view.StartScreen;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        StartScreen ss = new StartScreen();
+//        ss.setVisible(true);
+          SignupScreen signup = new SignupScreen();
+          signup.setVisible(true);
+          
+
+    }
+    
+}
